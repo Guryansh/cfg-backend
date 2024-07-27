d2280ccd11b297d54f79145df56e3bd22bfd6f06
Niharika Aggarwal <niharikaagg04@gmail.com>
2024-07-27 19:19:48 +0530

    added circle leader model

---
+++ src/models/circleLeader.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+// Define the schema for the CircleLeader
+const CircleLeaderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  workLocation: {
+    type: String,
+    required: true,
+  },
+  volunteers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Volunteer', // Assuming you have a Volunteer model
+  }],
+  attendance: [{
+    date: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['Present', 'Absent', 'Late'],
+      required: true,
+    },
+  }],
+}, {
+  timestamps: true,
+});
+
+// Pre-save middleware to hash password
+CircleLeaderSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+
+module.exports = mongoose.model('CircleLeader', CircleLeaderSchema)
